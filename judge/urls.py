ca2a82d5aa923c467e4e9444545a0bbc899316a1
Arjav Jain <luckyjain1271@gmail.com>
2022-07-09 03:28:18 +0530

    Skeletel Done

--- judge/urls.py
+++ judge/urls.py
@@ -5,5 +5,5 @@ app_name = 'judge'
 urlpatterns = [
     path('', views.problems, name = 'problems'),
     path('problem/<int:problem_id>/', views.problemDetail, name ='problem_detail'),
-    
+    path('problem/<int:problem_id>/submit/', views.problemDetail , name='submit'),
 ]
